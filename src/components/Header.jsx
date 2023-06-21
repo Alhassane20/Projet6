e511e5b9f7e8945999782058d5164df1917832c1
Alhassane Seye <seyealhassane@gmail.com>
2023-06-21 15:39:09 +0200

    save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import {Link} from 'react-router-dom'
+import header from './images/header.png'
+import header2 from './images/header2.png'
+import header3 from './images/header3.png'
+import header4 from './images/header4.png'
+import './style/Header.sass'
 
 function Header() {
     return(
         <nav>
-        <Link to='/'>Accueil</Link>
-        <Link to='about'>A propos</Link>
+            <div id='logo'>
+                <img src={header} alt="lettre K"/>
+                <img src={header2} alt="lettre A"/>
+                <img src={header3} alt="lettre S"/>
+                <img src={header4} alt="lettre A"/>
+            </div>
+            <div id='liens'>
+                <Link to='/'>Accueil</Link>
+                <Link to='about'>A propos</Link>
+            </div>
         </nav>
     )
 }
