84196f28d17c268df8714d7ae090ce4f44f00911
Alhassane Seye <seyealhassane@gmail.com>
2023-11-28 22:28:13 +0100

    maj

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import CarrouselComponent from './components/Carrousel.jsx'
 function AppRouter() {
 
   return(
+    <BrowserRouter basename="/Projet8">
     <Router>
         <Routes>
             <Route path='/' element={<Home />}/>
@@ -23,6 +24,7 @@ function AppRouter() {
 
             </Routes>
             </Router>
+            </BrowserRouter>
         );
 }
 
