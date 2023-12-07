c3380ae20d11bde9aa0075c027a2fc326c46e0fc
Alhassane Seye <seyealhassane@gmail.com>
2023-12-07 19:56:35 +0100

    maj

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import CarrouselComponent from './components/Carrousel.jsx'
 function AppRouter() {
 
   return(
-    <Router basename="/Projet6">
+    <Router basename={process.env.PUBLIC_URL}>
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='*' element={<Error404 />}/>
